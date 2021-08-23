ec583de9c61a5ca69199f96768f084f6e6a68553
moruno21 <amoruno21@gmail.com>
2021-08-23 17:35:52 +0200

    comments added

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,4 @@
-def printWelcome():
+def printWelcome():  # Function that prints at the start of the game
     print("Welcome to the tic-tac-toe game.  I wish you enjoy it, let's get started!")
     print("Those are all the positions:\n")
     print("[ 1 ][ 2 ][ 3 ]")
@@ -6,13 +6,13 @@ def printWelcome():
     print("[ 7 ][ 8 ][ 9 ]\n")
 
 
-def printBoard():
+def printBoard():  # Prints the game board
     print('[' + board["1"] + '][' + board["2"] + '][' + board["3"] + ']')
     print('[' + board["4"] + '][' + board["5"] + '][' + board["6"] + ']')
     print('[' + board["7"] + '][' + board["8"] + '][' + board["9"] + ']\n')
 
 
-def checkIfTicTacToe():
+def checkIfTicTacToe():  # Checks if there are 3 positions together with the same value and if it isn't ' '
     if(board["1"] == board["2"] and board["2"] == board["3"] and board["3"] != ' '):
         return True
     if(board["4"] == board["5"] and board["5"] == board["6"] and board["6"] != ' '):
@@ -31,28 +31,29 @@ def checkIfTicTacToe():
         return True
 
 
+# The game starts
 printWelcome()
 
 board = {"1": ' ', "2": ' ', "3": ' ', "4": ' ',
-         "5": ' ', "6": ' ', "7": ' ', "8": ' ', "9": ' '}
+         "5": ' ', "6": ' ', "7": ' ', "8": ' ', "9": ' '}  # Dictionary to store the value of each position
 
 x = True  # Stablish whether it's X's or O's turn
 
-while(True):
-    if(x == True):
+while(True):  # It only stops when someone wins
+    if(x == True):  # X's turns starts
         position = input("X's turn, choose a position: ")
         board[position] = 'X'
         x = False  # O's turn
         printBoard()
-    else:
+    else:  # O's turns starts
         position = input("O's turn, choose a position: ")
         board[position] = 'O'
         x = True  # X's turn
         printBoard()
-    if checkIfTicTacToe():
+    if checkIfTicTacToe():  # After each turn, we check if there's a tic-tac-toe
         break
 
-if x == True:
+if x == True:  # Prints who's the winner
     print("O wins!")
 else:
     print("X wins!")
